42418ea603964ff47065d5d5b480b10352c93013
Josh Turan <joshturan@Joshs-MacBook-Air.local>
2019-08-02 06:30:24 +0200

    Step 2 Prevent name repeats

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', id: 0 }
   ])
 
   const [newName, setNewName] = useState('')
@@ -14,24 +14,32 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+
+
   const addName = (event) => {
     event.preventDefault()
-    const personsObject = {
-      name: newName,
-      id: persons.length + 1
-    }
-    setPersons(persons.concat(personsObject))
-    setNewName('')
 
+    // Check to see if name exists, if not add name
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const personsObject = {
+        name: newName,
+        id: persons.length + 1
+      }
+      setPersons(persons.concat(personsObject))
+      setNewName('')
+    }
   }
+
+
 
   const personsName = () => {
     return (
-      persons.map(person => <p id={person.id}>{person.name}</p>)
+      persons.map(person => <p key={person.id}>{person.name}</p>)
     )
   }
 
-  console.log('personName', personsName())
 
   return (
     <div>
@@ -45,9 +53,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-
       {personsName()}
-
     </div>
   )
 }
