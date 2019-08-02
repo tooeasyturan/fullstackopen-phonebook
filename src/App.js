4c043b80c4c440a4946508ff9bbf30330f0e7ec5
Josh Turan <joshturan@Joshs-MacBook-Air.local>
2019-08-02 04:32:02 +0200

    Step 1 add names to phonebook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React, { useState } from 'react'
 
+
 const App = () => {
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ])
+
   const [newName, setNewName] = useState('')
+
+  const handleChangeName = (event) => {
+    //console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const personsObject = {
+      name: newName,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(personsObject))
+    setNewName('')
+
+  }
+
+  const personsName = () => {
+    return (
+      persons.map(person => <p id={person.id}>{person.name}</p>)
+    )
+  }
+
+  console.log('personName', personsName())
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChangeName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+
+      {personsName()}
+
     </div>
   )
 }
